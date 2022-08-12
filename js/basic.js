bdf1897a4f4baaad1a83ec1bc1f65aeee40b002e
Shadab Ansari <ahmad.shadab-ansari@capgemini.com>
2022-08-12 09:32:20 +0530

    added

---
+++ js/basic.js
@@ -0,0 +1,17 @@
+//date 
+const date = new Date();
+
+console.log(date) ; // current date 
+console.log(date.getDate()) ;
+console.log(date.getDay()) ;// 0-sunday 6-saturday
+console.log(date.getTime()) ; // time in milliseconds
+
+//math
+console.log(Math.PI) ; //3.14
+console.log(Math.pow(2,3)) ;//8
+console.log(Math.sqrt(144)) ;//12
+console.log(Math.random());
+
+console.log(Math.ceil(4.3)) ;//5
+console.log(Math.floor(4.3)) ;//4
+
